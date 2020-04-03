04c8a6e333580f6db29ab0711705974bf31d7437
MemedSalmon <william.almen22@gmail.com>
2020-04-03 17:05:42 +0200

    First

---
+++ VideoUploader/Models/VideoData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace VideoUploader.Models
+{
+    public class VideoData
+    {
+        [JsonPropertyName("Title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("Path")]
+        public string Path { get; set; }
+
+        [JsonPropertyName("Description")]
+        public string Description { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize(this);
+    }
+}
